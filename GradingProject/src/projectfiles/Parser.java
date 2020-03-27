481073d75c23aa473ef77aa49379044673667727
Claire McShane <cemcshane@wustl.edu>
2020-03-27 17:40:04 -0400

    working

--- GradingProject/src/projectfiles/Parser.java
+++ GradingProject/src/projectfiles/Parser.java
@@ -1,10 +1,19 @@
 package projectfiles;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Parser {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws FileNotFoundException { //might need to change to try/catch 
+		// TODO Auto-generated method stub								  //around the scanner declaration
+		File file = new File(args[0]);
+		@SuppressWarnings("resource")
+		Scanner sc = new Scanner(file);
+		while(sc.hasNextLine()) {
+			System.out.println(sc.nextLine());
+		}
 	}
 
 }
