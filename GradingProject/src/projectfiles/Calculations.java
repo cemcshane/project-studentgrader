1070340a7ad8e186fa0ad1d1e841f72291d6ed0e
cmcvengros <54557322+cmcvengros@users.noreply.github.com>
2020-04-17 16:47:18 -0500

    preliminary calculations

--- GradingProject/src/projectfiles/Calculations.java
+++ GradingProject/src/projectfiles/Calculations.java
@@ -1,6 +1,29 @@
 package projectfiles;
 
+import java.util.LinkedList;
+
 public class Calculations {
+
+	double pointsAwarded;
+	double pointsPossible;
+	GradingCategory category;
+	String name;
+	int percentage;
+	LinkedList<Assignment> assignments;
+
+
+	//import classes from Parser, Assignment, and GradingCategory
+	Parser finalGrade = new Parser();
+	Assignment newAssign = new Assignment(pointsAwarded, pointsAwarded, category);
+	GradingCategory newCat = new GradingCategory(name, percentage);
 	
+	//
 	
+
+	
+
+
 }
+
+
+
