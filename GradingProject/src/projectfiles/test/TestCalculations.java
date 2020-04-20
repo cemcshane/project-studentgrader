0551bd1db74686d9c913aa9052d465d59cc70080
arielhaberman <46663812+arielhaberman@users.noreply.github.com>
2020-04-19 22:06:29 -0400

    Calculations Test Done

--- GradingProject/src/projectfiles/test/TestCalculations.java
+++ GradingProject/src/projectfiles/test/TestCalculations.java
@@ -11,44 +11,37 @@ import projectfiles.Parser;
 
 class TestCalculations {
 
-	Parser input = new Parser();
-	Calculations calc = new Calculations(input);
-	GradingCategory catOne = new GradingCategory("one", 50);
-	GradingCategory catTwo = new GradingCategory("two", 25);
-	GradingCategory catThree = new GradingCategory("three", 25);
-	Assignment assOne = new Assignment(10, 20, catOne);
-	Assignment assTwo = new Assignment(15, 20, catOne);
-	Assignment assThree = new Assignment(20, 20, catOne);
-	Assignment assFour = new Assignment(30, 40, catTwo);
-	Assignment assFive = new Assignment(50, 50, catTwo);
-	Assignment assSix = new Assignment(10, 10, catThree);
-	
-	
+
 	@Test
-	void testFinalCalculationSimple() {
-		input.addCategory(catOne);
-		input.addCategory(catTwo);
-		input.addCategory(catThree);
-		input.addAssignment("catOne", assThree);
-		input.addAssignment("catTwo", assFive);
-		input.addAssignment("catThree", assSix);
+	void testFinalCalculationSortaSimple() {
+		Parser input = new Parser();
+		GradingCategory catFive = new GradingCategory("five", 100);
+		input.addCategory(catFive);
+		Assignment assSeven = new Assignment(100, 100, catFive);
+		Assignment assEight = new Assignment(100, 100, catFive);
+		Calculations calc = new Calculations(input);
 		double returnValue = calc.finalCalculation();
 		assertEquals(100, returnValue);
 	}
 	
 	@Test
 	void testFinalCalculation() {
+		Parser input = new Parser();
+		GradingCategory catOne = new GradingCategory("one", 50);
+		GradingCategory catTwo = new GradingCategory("two", 25);
+		GradingCategory catThree = new GradingCategory("three", 25);
 		input.addCategory(catOne);
 		input.addCategory(catTwo);
 		input.addCategory(catThree);
-		input.addAssignment("catOne", assOne);
-		input.addAssignment("catOne", assTwo);
-		input.addAssignment("catOne", assThree);
-		input.addAssignment("catTwo", assFour);
-		input.addAssignment("catTwo", assFive);
-		input.addAssignment("catThree", assSix);
+		Assignment assOne = new Assignment(10.0, 20.0, catOne);
+		Assignment assTwo = new Assignment(15.0, 20.0, catOne);
+		Assignment assThree = new Assignment(20.0, 20.0, catOne);
+		Assignment assFour = new Assignment(30.0, 40.0, catTwo);
+		Assignment assFive = new Assignment(50.0, 50.0, catTwo);
+		Assignment assSix = new Assignment(10.0, 10.0, catThree);
+		Calculations calc = new Calculations(input);
 		double returnValue = calc.finalCalculation();
-		assertEquals((25+(((75+100)/2)*.25)+(((50+75+100)/3)*.5)), returnValue,.01);
+		assertEquals((25+(((80.0)/90.0)*25)+(((45.0)/60.0)*50)), returnValue,.01);
 	}
 
 }
